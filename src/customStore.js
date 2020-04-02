96f9ea0d0c6f2030e2f317657c9ac1f187020afe
Chung Thế Hào <chungthehao@gmail.com>
2020-04-02 18:51:40 +0700

    Subscribing to the Store

--- src/customStore.js
+++ src/customStore.js
@@ -2,6 +2,7 @@ import reducer from "./reducer";
 
 function createStore(reducer) {
   let state; // private, vì không bị expose ra ngoài.
+  let listeners = []; // Có thể có nhiều listeners từ nhiều UI components khác nhau
 
   function getState() {
     return state;
@@ -10,11 +11,19 @@ function createStore(reducer) {
   function dispatch(action) {
     // 1. Call the reducer to get the new state
     state = reducer(state, action);
+
+    // 2. state thay đổi rồi đó ~ store changed!
+    for (let i = 0; i < listeners.length; i++) listeners[i]();
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
   }
 
   return {
     getState,
-    dispatch
+    dispatch,
+    subscribe
   };
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import store from "./customStore";
 import * as actions from "./actions";
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log("Store changed!");
+});
+
+console.log("Store not changed yet!");
 
 store.dispatch(actions.bugAdded("A new bug"));
-
-console.log(store.getState());
