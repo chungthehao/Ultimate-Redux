fef503484dba69b15adec31a8aa6580244ca9fb3
Chung Thế Hào <chungthehao@gmail.com>
2020-04-02 16:28:24 +0700

    Dispatching Actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hi Henry!");
+import store from "./store";
+
+// Dispatch 'bugAdded' action
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1"
+  }
+});
+
+// Xem thử state hiện tại trong store
+console.log(store.getState());
+
+// Dispatch 'bugRemoved' action
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1
+  }
+});
+
+// Xem thử state hiện tại trong store
+console.log(store.getState());
