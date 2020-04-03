384f9a6387f03bfc2c96060be7c627b68eddd5bf
Chung Thế Hào <chungthehao@gmail.com>
2020-04-03 23:40:21 +0700

    Creating the Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 
 import reducer from "./bugs";
 
 export default function() {
-  return createStore(reducer, devToolsEnhancer({ trace: true }));
+  //return createStore(reducer, devToolsEnhancer({ trace: true }));
+  return configureStore({ reducer });
 }
