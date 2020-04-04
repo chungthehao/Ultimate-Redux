cd260a262aaed6fe996ec26b3b05d97433f2b206
Chung Thế Hào <chungthehao@gmail.com>
2020-04-04 09:23:08 +0700

    Creating Slices

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -2,47 +2,69 @@
 // 1. Reducer phải là default export từ module này.
 // 2. Phải export mỗi action creator ra ngoài.
 
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// const hamTaoLao = createAction("ABC"); // type -> 'ABC'
-// console.log(hamTaoLao({ id: 321 })); // { type: 'ABC', payload: { id: 321 } }
-// console.log(hamTaoLao.type); // ABC
-// console.log(hamTaoLao.toString()); // ABC
+let lastId = 0;
+
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        resolved: false,
+        description: action.payload.description
+      });
+    },
+    bugRemoved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs.splice(index, 1);
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    }
+  }
+});
+
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
 
 /**
  * Action Creators
  */
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
+// export const bugAdded = createAction("bugAdded");
+// export const bugRemoved = createAction("bugRemoved");
+// export const bugResolved = createAction("bugResolved");
 
 /**
  * Reducer
  */
-let lastId = 0;
+
 // Reducer in Redux have to be a pure function.
 
-export default createReducer([], {
-  // key: value
-  // actions: functions ~ (event: event handler)
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      resolved: false,
-      description: action.payload.description
-    });
-  },
+// export default createReducer([], {
+//   // key: value
+//   // actions: functions ~ (event: event handler)
+//   [bugAdded.type]: (bugs, action) => {
+//     bugs.push({
+//       id: ++lastId,
+//       resolved: false,
+//       description: action.payload.description
+//     });
+//   },
 
-  [bugRemoved.type]: (bugs, action) => {
-    const index = bugs.findIndex(bug => bug.id === action.payload.id);
-    bugs.splice(index, 1);
-  },
+//   [bugRemoved.type]: (bugs, action) => {
+//     const index = bugs.findIndex(bug => bug.id === action.payload.id);
+//     bugs.splice(index, 1);
+//   },
 
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex(bug => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  }
-});
+//   [bugResolved.type]: (bugs, action) => {
+//     const index = bugs.findIndex(bug => bug.id === action.payload.id);
+//     bugs[index].resolved = true;
+//   }
+// });
 
 // export default function reducer(state = [], action) {
 //   switch (action.type) {
