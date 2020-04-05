77315c70dd0b7a051785223aa2289413c31fcb51
Chung Thế Hào <chungthehao@gmail.com>
2020-04-05 20:39:26 +0700

    toast middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,11 +4,16 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import reducer from "./reducer";
 import logger from "../middleware/logger";
+import toast from "../middleware/toast";
 
 export default function() {
   //return createStore(reducer, devToolsEnhancer({ trace: true }));
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger({ destination: "console" })]
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({ destination: "console" }),
+      toast
+    ]
   });
 }
