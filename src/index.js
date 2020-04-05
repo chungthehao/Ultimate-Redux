869c4360067143883c7fba454faa41863abb38d8
Chung Thế Hào <chungthehao@gmail.com>
2020-04-05 16:36:13 +0700

    Assign a bug to a user, get bugs by userId

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import configureStore from "./store/configureStore";
-import { bugAdded, bugResolved, getUnresolvedBugs } from "./store/bugs";
+import {
+  bugAdded,
+  bugResolved,
+  bugAssignedToUser,
+  getUnresolvedBugs,
+  getBugsByUser
+} from "./store/bugs";
 import { projectAdded } from "./store/projects";
+import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-// *** PROJECT ***
+// *** USERS ***
+store.dispatch(userAdded({ name: "User 1" }));
+store.dispatch(userAdded({ name: "User 2" }));
+
+// *** PROJECTS ***
 store.dispatch(projectAdded({ name: "Project 1" }));
 
 // *** BUGS ***
@@ -14,6 +25,8 @@ store.dispatch(bugAdded({ description: "Bug 3" }));
 
 store.dispatch(bugResolved({ id: 1 }));
 
-const unresolvedBugs1 = getUnresolvedBugs(store.getState());
-const unresolvedBugs2 = getUnresolvedBugs(store.getState());
-console.log(unresolvedBugs1 === unresolvedBugs2);
+store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
+store.dispatch(bugAssignedToUser({ bugId: 3, userId: 1 }));
+
+const user1Bugs = getBugsByUser(1)(store.getState());
+console.log(user1Bugs);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -24,11 +24,21 @@ const slice = createSlice({
     bugResolved: (bugs, action) => {
       const index = bugs.findIndex(bug => bug.id === action.payload.id);
       bugs[index].resolved = true;
+    },
+    bugAssignedToUser: (bugs, action) => {
+      const { userId, bugId } = action.payload;
+      const index = bugs.findIndex(b => b.id === bugId);
+      bugs[index].userId = userId;
     }
   }
 });
 
-export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export const {
+  bugAdded,
+  bugRemoved,
+  bugResolved,
+  bugAssignedToUser
+} = slice.actions;
 export default slice.reducer;
 
 /**
@@ -37,11 +47,16 @@ export default slice.reducer;
  * Param cuối là 1 hàm result function
  * Lợi ích của việc cache: Ko tính toán lại nếu input ko đổi + trả về giá trị cũ (memory cũ) nên ko trigger re-render trong React
  */
+const getBugs = state => state.entities.bugs;
+
 export const getUnresolvedBugs = createSelector(
-  state => state.entities.bugs,
+  getBugs,
   state => state.entities.projects,
   (bugs, projects) => bugs.filter(b => !b.resolered) // Nếu bugs, projects ko đổi thì [[bugs.filter(b => !b.resolered)]] ko chạy lại mà lấy từ cache
 );
+
+export const getBugsByUser = userId =>
+  createSelector(getBugs, bugs => bugs.filter(b => b.userId === userId));
 
 // Selector (1 hàm nhận state => computed state)
 // export const getUnresolvedBugs = state =>
