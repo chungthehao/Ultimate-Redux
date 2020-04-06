9f2a2f71af5f7e0a019303f39443b5b4c77be459
Chung Thế Hào <chungthehao@gmail.com>
2020-04-07 00:22:09 +0700

    Your First Unit Test

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,17 @@
-it("my first test name", () => {});
+import { isEven } from "./math";
+
+describe("isEven", () => {
+  // it(tên_của_test, test_function)
+  it("should return true if given an even number", () => {
+    // Function under test ~ System under test (SUT)
+    const result = isEven(2);
+
+    // Make an assertion
+    expect(result).toEqual(true);
+  });
+
+  it("should return false if given an odd number", () => {
+    const result = isEven(1);
+    expect(result).toEqual(false);
+  });
+});
